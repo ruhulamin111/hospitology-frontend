053aa8a3ef7f224337a9c56a7a32eb652f6e6abe
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-11-05 12:39:33 +0600

    header complete

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <div className="shadow-md ">
-            <div className='navbar h-20 w-11/12 mx-auto'>
+            <div className='navbar h-20 w-11/12 mx-auto font-bold'>
                 <div className="navbar-start">
 
                     {/* small screen option  */}
@@ -13,45 +14,145 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+                            <li >
+                                <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                    Home
+                                </Link>
+                            </li>
                             <li tabIndex={0}>
-                                <a className="justify-between">
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
+                                <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                    Doctors
+                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                                </Link>
                                 <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Department wise
+                                        </Link>
+                                    </li>
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Branch wise
+                                        </Link>
+                                    </li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li >
+                                <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                    Patient care
+                                </Link>
+                            </li>
+                            <li tabIndex={0}>
+                                <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                    Our services
+                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                                </Link>
+                                <ul className="p-2">
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Hospital service
+                                        </Link>
+                                    </li>
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Diagonistic servic
+                                        </Link>
+                                    </li>
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Dental service
+                                        </Link>
+                                    </li>
+                                    <li >
+                                        <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                            Health checkup package
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li >
+                                <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                    About us
+                                </Link>
+                            </li>
                         </ul>
                     </div>
-                    <Link to='/' className="normal-case text-xl">Hospitology</Link>
+                    <Link to='/' className="normal-case text-3xl text-secondary hover:text-primary duration-300 ease-in">Hospitology</Link>
                 </div>
 
                 {/* large screen option  */}
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>Item 1</a></li>
+                        <li >
+                            <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                Home
+                            </Link>
+                        </li>
                         <li tabIndex={0}>
-                            <a>
-                                Parent
+                            <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                Doctors
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
+                            </Link>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Department wise
+                                    </Link>
+                                </li>
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Branch wise
+                                    </Link>
+                                </li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li >
+                            <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                Patient care
+                            </Link>
+                        </li>
+                        <li tabIndex={0}>
+                            <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                Our services
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                            </Link>
+                            <ul className="p-2">
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Hospital service
+                                    </Link>
+                                </li>
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Diagonistic servic
+                                    </Link>
+                                </li>
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Dental service
+                                    </Link>
+                                </li>
+                                <li >
+                                    <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                        Health checkup package
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li >
+                            <Link className='duration-300 ease-in hover:text-secondary' to='/'>
+                                About us
+                            </Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Get started</a>
+                    <Link to='/' className="text-3xl text-secondary p-2 border rounded-full border-secondary hover:bg-secondary hover:text-white duration-300 ease-in">
+                        <FaUserCircle />
+                    </Link>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
